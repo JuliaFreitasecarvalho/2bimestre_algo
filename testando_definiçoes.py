7a31843060a12d94e5d6436d78d4a4315ef5dc78
Júlia Freitas e Carvalho <gatusdaju@gmail.com>
2025-05-26 15:24:19 -0400

    Add files via upload

--- testando_definiçoes.py
+++ testando_definiçoes.py
@@ -1,18 +1,41 @@
-compras = ["Pão", "Café", "Leite"]
-print(compras[0])
+# compras = ["Pão", "Café", "Leite"]
+# print(compras[0])
 
-compras.remove(compras[2]) #Pode ser removido pelo nome ou pelo indece
-print(compras)
+# compras.remove(compras[2]) #Pode ser removido pelo nome ou pelo indece
+# print(compras)
 
-compras.append("Açucar") #Append acrescenta um item ao final da lista, podendo adicionar um por vez
-print(compras)
+# compras.append("Açucar") #Append acrescenta um item ao final da lista, podendo adicionar um por vez
+# print(compras)
 
-compras_ordenadas = sorted(compras) # É preciso criar uma lista nova para receber os elementos ordenados na lista antiga 
-print(compras_ordenadas)
+# compras_ordenadas = sorted(compras) # É preciso criar uma lista nova para receber os elementos ordenados na lista antiga 
+# print(compras_ordenadas)
 
-for item in compras:
-    print("-",item) # Tanto faz o nome que estiver dentro, pois ele é apenas um identificador 
+# for item in compras:
+#     print("-",item) # Tanto faz o nome que estiver dentro, pois ele é apenas um identificador 
 
 
 
- 
+
+
+#listas []
+#Dicionários {Chave valor}
+#Tuplas ()
+#Conjuntos set()
+
+#lista
+# teste = [1, 2, 3,] 
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+# #Tuplas
+# teste = (1, 2, 3,)
+# print(teste)
+# teste.remove(1) #Não dá para remover ou editar, pois são tuplas, desde o momento que são adicionadas não poderam saõ alteradas
+# print(teste)
+
+#Comjunto
+teste = {1, 2, 3,2, 2} #Não repete todos os item duplicados, apenas uma vez
+print(teste)
+# teste.remove(2)
+# print(teste)
